90a1b7d071f767859f66c9a1b85d82d6998def48
VancoriComplex <wrobel_mat@icloud.com>
2020-12-01 09:42:43 +0100

    solution Day1: Part1 + Part2

--- com.mateusz.aoc2020/src/main/java/TestAdvent.java
+++ com.mateusz.aoc2020/src/main/java/TestAdvent.java
@@ -1,5 +1,19 @@
+import day1.Day1;
+import util.HttpsReader;
+
+import java.io.IOException;
+import java.net.URL;
+
 public class TestAdvent {
     public static void main(String[] args) {
-
+        String input = null;
+        try {
+            input = HttpsReader.getContent(new URL("someUrl"), "sessionKey");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assert input != null;
+        Integer result = Day1.Part2.processExpenseReport(input, 2020);
+        System.out.println(result);
     }
 }
